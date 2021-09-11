2067f7f3dabee0ce29e316b9cd8daff057efa946
felipe2110 <jfcontreras66@misena.edu.co>
2021-09-11 15:54:49 -0500

    Updates

--- src/EmptyTodos/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './EmptyTodos.css';
-
-function EmptyTodos(){
-    return(
-        <p className="EmptyTodos">¡Crea tu primer Todo!</p>
-    )
-}
-export {EmptyTodos};
--- src/TodosError/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function TodosError ({error}){
-    return <p>Desespérate, hubo un error ...</p>
-}
-export {TodosError};
